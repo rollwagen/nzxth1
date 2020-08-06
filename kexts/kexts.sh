be65181b81c3b112f54f10a80f37bb659ded858c
rollwagen <rollwagen@gmail.com>
2020-08-06 20:36:57 +0200

    Added authenticated requests to github api.

--- kexts/kexts.sh
+++ kexts/kexts.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-function print_info {
+print_info () {
 	echo "$(tput bold)$1$(tput sgr 0)"
 }
 
+curl_url () {
+	if [[ -z "${GITHUB_CLIENT_ID}" ]]; then
+		# NOT defined
+		echo `curl -s $1 | grep "browser_download_url" | grep "RELEASE" | cut -d : -f 2,3 | tr -d \ \"`
+	else
+		# defined - for authenticated requests, see https://developer.github.com/v3/#rate-limiting
+		echo `curl -u $GITHUB_CLIENT_ID:$GITHUB_CLIENT_SECRET -s $1 | grep "browser_download_url" | grep "RELEASE" | cut -d : -f 2,3 | tr -d \ \"`
+	fi
+
+}
+
+
 print_info "Downloading and unzipping latest 'IntelMausi' kext release..."
-download_url=`curl -s https://api.github.com/repos/acidanthera/IntelMausi/releases/latest | grep "browser_download_url" | grep "RELEASE" | cut -d : -f 2,3 | tr -d \ \"`
+download_url=`curl_url https://api.github.com/repos/acidanthera/IntelMausi/releases/latest`
 version=`echo $download_url | awk -F\/ '{print $8}'`
 print_info "VERSION: $version"
-#curl -s https://api.github.com/repos/acidanthera/IntelMausi/releases/latest | grep "browser_download_url" | grep "RELEASE" | cut -d : -f 2,3 | tr -d \ \" | xargs curl -OL
 curl -OL "$download_url"
 unzip -d intelmausi-$version IntelMausi*.zip
 rm IntelMausi*.zip
 
-
 print_info "Downloading and unzipping latest 'Lilu' kext release..."
-download_url=`curl -s https://api.github.com/repos/acidanthera/Lilu/releases/latest | grep "browser_download_url" | grep "RELEASE" | cut -d : -f 2,3 | tr -d \ \"`
+download_url=`curl_url https://api.github.com/repos/acidanthera/Lilu/releases/latest`
 version=`echo $download_url | awk -F\/ '{print $8}'`
 print_info "VERSION: $version"
 curl -OL "$download_url"
