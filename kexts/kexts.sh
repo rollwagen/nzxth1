2a2a4cb9029bb650734ddd2013ddb57d81adc013
rollwagen <rollwagen@gmail.com>
2020-08-11 16:35:05 +0200

    Added cleanup option.

--- kexts/kexts.sh
+++ kexts/kexts.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+# option "-c" will cleanup i.e. delete all downloaded content
+while getopts 'c' option
+do
+  case $option in
+    c) ACTION=CLEANUP ;;
+  esac
+done
+
 print_info () {
 	echo "$(tput bold)$1$(tput sgr 0)"
 }
 
-print_info "INFO: consider setting GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET for authenticated api requests (see https://developer.github.com/v3/#rate-limiting)"
 curl_url () {
 	if [[ -z "${GITHUB_CLIENT_ID}" ]]; then
+		print_info "INFO: consider setting GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET for authenticated api requests (see https://developer.github.com/v3/#rate-limiting)"
 		echo `curl -s $1  | jq '.assets[] | select(.browser_download_url | contains("RELEASE")) | .browser_download_url' | tr -d \ \" `
 	else
 		echo `curl -u $GITHUB_CLIENT_ID:$GITHUB_CLIENT_SECRET -s $1  | jq '.assets[] | select(.browser_download_url |contains("RELEASE")) | .browser_download_url' | tr -d \ \"`
 	fi
 
 }
+
+if [ "$ACTION" = "CLEANUP" ]; then
+	print_info "Cleaning up (deleting all downloaded content)."
+	rm -rf intelmausi-* lilu-* virtualsmc-* whatevergreen-* itlwm*
+	exit
+fi
 
 
 print_info "Downloading and unzipping latest 'WhateverGreen' kext release..."
@@ -57,4 +71,3 @@ git clone https://github.com/OpenIntelWireless/itlwm.git
 
 
 
-# rm -rf intelmausi-* lilu-* virtualsmc-* whatevergreen-* itlwm*
