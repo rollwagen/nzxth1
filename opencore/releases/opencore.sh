63633553a708c4d5e1fcacb2d15e55f52f3e1633
rollwagen <rollwagen@gmail.com>
2020-08-11 13:33:46 +0200

    Using jq for json select.

--- opencore/releases/opencore.sh
+++ opencore/releases/opencore.sh
@@ -6,7 +6,13 @@ function print_info {
 
 
 print_info "Downloading and unzipping latest 'OpenCore' release..."
-download_url=`curl -s https://api.github.com/repos/acidanthera/OpenCorePkg/releases/latest | grep "browser_download_url" | grep "RELEASE" | cut -d : -f 2,3 | tr -d \ \"`
+download_url=""
+if [[ -z "${GITHUB_CLIENT_ID}" ]]; then
+	download_url=`curl -s https://api.github.com/repos/acidanthera/OpenCorePkg/releases/latest | jq '.assets[] | select(.browser_download_url | contains("RELEASE")) | .browser_download_url' | tr -d \ \" `
+else
+	download_url=`curl -u $GITHUB_CLIENT_ID:$GITHUB_CLIENT_SECRET -s https://api.github.com/repos/acidanthera/OpenCorePkg/releases/latest | jq '.assets[] | select(.browser_download_url | contains("RELEASE")) | .browser_download_url' | tr -d \ \" `
+fi
+
 version=`echo $download_url | awk -F\/ '{print $8}'`
 print_info "VERSION: $version"
 curl -OL "$download_url"
