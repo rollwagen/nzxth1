db29e512534959d30242f201c0e0320a71e35438
rollwagen <rollwagen@gmail.com>
2020-08-11 16:52:54 +0200

    Added cleanup option.

--- tools/tools.sh
+++ tools/tools.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# option "-c" will cleanup i.e. delete all downloaded content
+while getopts 'c' option
+do
+  case $option in
+    c) ACTION=CLEANUP ;;
+  esac
+done
+
 function print_info {
 	echo "$(tput bold)$1$(tput sgr 0)"
 }  
+
+if [ "$ACTION" = "CLEANUP" ]; then
+        print_info "Cleaning up (i.e. deleting all downloaded content in this folder)."
+	print_info "To uninstall hackingtool execute 'brew cask uninstall hackintool'."
+	rm -rf GenSMBIOS/  MountEFI/ ProperTree/ gibMacOS/ MaciASL* OCConfigCompare*
+        exit
+fi
+
 
 print_info "Cloning github based tools..."
 
@@ -23,16 +39,20 @@ git clone https://github.com/corpnewt/OCConfigCompare
 
 
 print_info "Downloading and unzipping MaciASL tool..."
-curl -OL https://github.com/acidanthera/MaciASL/releases/download/1.5.7/MaciASL-1.5.7-RELEASE.zip
-unzip -d MaciASL MaciASL-*.zip
+curl -s -OL https://github.com/acidanthera/MaciASL/releases/download/1.5.7/MaciASL-1.5.7-RELEASE.zip
+unzip -q -d MaciASL MaciASL-*.zip
 rm MaciASL-*.zip
 
 
 print_info "Installing Hackingtool via 'brew' (source available at https://github.com/headkaze/Hackintool)..."
-brew cask install hackintool
+hackintool_installed=`brew cask list --json hackintool | jq '.[]|.token'`
+if [ $hackintool_installed = "\"hackintool\"" ]; then
+	print_info "Hackintool already installed."
+else
+	brew cask install hackintool
+fi
 
 # IORegistryExplorer
 # https://mac.filehorse.com/download-ioregistryexplorer/
 # https://mac.filehorse.com/download-ioregistryexplorer/
 
-# rm -rf GenSMBIOS/  MountEFI/ ProperTree/ gibMacOS/ MaciASL* OCConfigCompare*
